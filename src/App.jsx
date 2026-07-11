1f4149189162061a3979bc5767686d695b33e796
saloni16527 <salonizoldyck@gmail.com>
2026-07-11 21:21:12 +0530

    remove

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,6 @@ import Welcomemsg from "./components/Welcomemsg";
 import { TodoItemsContext } from "./store/todo-items-store";
 
 function App(){ 
-   /* const todoItems =[
-      {
-        name:"Buy Milk",
-        dueDate: "4/10/2026",
-      },
-      {
-        name:"Go to college",
-        dueDate:"4/10/2026",
-      },
-      {
-        name:"like this video",
-        dueDate:"right now",
-      },
-    ];*/
 
  const [todoItems , setTodoItems] = useState ([]);
 
@@ -33,18 +19,6 @@ function App(){
       ];
       setTodoItems(newTodoItems);
 
-
-     /* setTodoItems((currValue) => {
-        const newTodoItems =[...currValue,
-      {name:itemName , dueDate :itemDueDate},
-      ];
-      return newTodoItems;
-      });
-
-
-      setTodoItems((currValue) => [...currValue,
-      {name:itemName , dueDate :itemDueDate},
-      ]);*/
   };
 
   const deleteItem = (todoItemName) => {
@@ -52,7 +26,6 @@ function App(){
      setTodoItems(newTodoItems);
   }
 
- /* const defaultTodoItems = [{name: 'buy ghee' , dueDate: 'Today'}];*/
 
       return (
       <TodoItemsContext.Provider value={{
